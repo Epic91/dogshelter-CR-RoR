ec35b53cb5effb439eee44dff17287d95b2a0114
Epic91 <hernandeze791@gmail.com>
2021-08-17 16:09:59 -0500

    used resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'dogs/index'
-  get 'dogs/new'
-  get 'dogs/create'
-  get 'dogs/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :dogs, only: [:index, :new, :create, :show]
 end
