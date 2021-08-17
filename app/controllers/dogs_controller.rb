05e1f4722b03933d6947ec0cc304aafaadb8ede2
Epic91 <hernandeze791@gmail.com>
2021-08-17 16:12:55 -0500

    added logic to methods in controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,13 +1,24 @@
 class DogsController < ApplicationController
   def index
+    @dogs = Dog.all
   end
 
   def new
   end
 
   def create
+    @dog = Dog.new
+    @dog[:name] = params[:dog][:name]
+    @dog[:breed] = params[:dog][:breed]
+    @dog[:gender] = params[:dog][:gender]
+    @dog[:age] = params[:dog][:age]
+    @dog[:description] = params[:dog][:description]
+    @dog.save
+
+    redirect_to dog_path(@dog)
   end
 
   def show
+    @dog = Dog.find(params[:id])
   end
 end
